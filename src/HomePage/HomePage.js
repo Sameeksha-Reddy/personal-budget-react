1b5a2b891efd067859ebe2bb8f16c6307fee0ac4
sameekksha <sameekshareddykv@gmail.com>
2023-10-01 18:56:48 -0400

    Personal Budget React All Changes Done

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import Chart from 'chart.js/auto';
+import * as d3 from 'd3';
+import '../App.scss'; 
+
+const colors = [
+    '#ff1a1a',
+    '#00cc44',
+    '#4d79ff',
+    '#e6e600',
+    '#8c1aff',
+    '#339933',
+    '#ac7339',
+    '#009999',
+    '#cc6699',
+    '#ff8533'
+];
+
+function BarChart() {
+    const svgRef = useRef();
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/budget')
+            .then(response => {
+                const data = response.data.myBudget;
+                const svg = d3.select(svgRef.current);
+                const width = 500;
+                const height = 300;
+                const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+                const innerWidth = width - margin.left - margin.right;
+                const innerHeight = height - margin.top - margin.bottom;
+
+                const x = d3.scaleBand()
+                    .domain(data.map(d => d.title))
+                    .range([0, innerWidth])
+                    .padding(0.1);
+
+
+                const y = d3.scaleLinear()
+                    .domain([0, d3.max(data, d => d.budget)])
+                    .nice()
+                    .range([innerHeight, 0]);
+
+                const g = svg.append('g')
+                    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+                    g.selectAll('.bar')
+                    .data(data)
+                    .enter()
+                    .append('rect')
+                    .attr('class', 'bar')
+                    .attr('x', d => x(d.title))
+                    .attr('y', d => y(d.budget))
+                    .attr('width', x.bandwidth())
+                    .attr('height', d => innerHeight - y(d.budget))
+                    .attr('fill', (d, i) => colors[i % colors.length]);
+
+                g.append('g')
+                    .attr('class', 'x-axis')
+                    .attr('transform', `translate(0,${innerHeight})`)
+                    .call(d3.axisBottom(x));
+
+                g.append('g')
+                    .attr('class', 'y-axis')
+                    .call(d3.axisLeft(y).ticks(null, 's'))
+                    .append('text')
+                    .attr('x', 2)
+                    .attr('y', y(y.ticks().pop()) + 0.5)
+                    .attr('dy', '0.32em')
+                    .attr('fill', '#000')
+                    .attr('font-weight', 'bold')
+                    .attr('text-anchor', 'start')
+                    .text('Budget');
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <svg ref={svgRef} width={500} height={300}></svg>
+    );
+}
+
 
 function HomePage() {
+    const [budgetData, setBudgetData] = useState([]);
+    const d3Container = useRef(null);
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/budget')
+            .then(response => {
+                const data = response.data.myBudget;
+                console.log('Data:', data);
+                setBudgetData(data);
+
+                if (chartRef.current) {
+                    chartRef.current.destroy();
+                }
+
+                const ctx = document.getElementById('Chart').getContext('2d');
+                chartRef.current = new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        labels: data.map(item => item.title),
+                        datasets: [{
+                            data: data.map(item => item.budget),
+                            backgroundColor: [
+                                '#ff1a1a',
+                                '#00cc44',
+                                '#4d79ff',
+                                '#e6e600',
+                                '#8c1aff',
+                                '#339933',
+                                '#ac7339',
+                                '#009999',
+                                '#cc6699',
+                                '#ff8533'
+                            ]
+                        }]
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []); 
+
   return (
 <main className='main'>
             <div className="text-box">
@@ -61,11 +188,15 @@ function HomePage() {
                 </p>
             </div>
     
-            <div className="text-box">
-                <h1>Charts</h1>
+            <div className="text">
+                <h1>Chartjs</h1>
                 <p>
-                    <canvas id="Chart" width="200" height="200"></canvas>
+                    <canvas id="Chart" width="50" height="50"></canvas>
                 </p>
+            </div>
+            <div ref={d3Container} className="text">
+                <h1>D3js Chart</h1>
+                <BarChart />
             </div>
 </main>
   );
